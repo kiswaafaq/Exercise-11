7324141728fbf6da132d0a35361215de1fe36300
kiswaafaq <kiswaafaque14@gmail.com>
2024-02-23 23:01:38 -0800

    first commit

---
+++ 11. Names.ts
@@ -0,0 +1,4 @@
+let names : string []= ['Kiswa', 'Uzma', 'Ajia', 'Iman'];
+for(let i=0; i<names.length; i++){
+console.log(names[i]);
+}
---
+++ names.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Kiswa', 'Uzma', 'Ajia', 'Iman'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
